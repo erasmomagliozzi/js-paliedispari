f484b758a76f4ff7b43fda3cf98969058e020a7b
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-13 21:35:27 +0100

    fine

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,15 +1,21 @@
 
-var parolaNuova = [''];
-var parola = prompt('Inserisci una parola');
-console.log(parola);
+
+var parolaUtente = prompt('Inserisci una parola');
+
+var parolaNuova = rovesciaParola(parolaUtente);
 
 
-for (var i = parola.length-1; i >= 0; i--) {
- parolaNuova[0] = parolaNuova[0] + parola[i];
-}
 
- if (parolaNuova[0] == parola){
+ if (parolaNuova== parolaUtente){
   console.log('Palindroma');
  }else{
   console.log('Non è palindroma');
  }
+
+function rovesciaParola (parola){
+  var parolaReverse = [''];
+  for (var i = parola.length-1; i >= 0; i--) {
+   parolaReverse += parola[i];
+  }
+  return parolaReverse;
+}
