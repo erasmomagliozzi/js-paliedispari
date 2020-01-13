00c511c4995955ad6144f39bf45209838ccf5d1c
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-13 17:17:29 +0100

    palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,6 +1,8 @@
+
 var parolaNuova = [''];
 var parola = prompt('Inserisci una parola');
 console.log(parola);
+
 
 for (var i = parola.length-1; i >= 0; i--) {
  parolaNuova[0] = parolaNuova[0] + parola[i];
